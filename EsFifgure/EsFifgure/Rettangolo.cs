960699366867d013bc42487299202192c96e04d1
unknown <luca.buttini@iisviolamarchesini.edu.it>
2024-03-25 17:03:33 +0100

    es figure

---
+++ EsFifgure/EsFifgure/Rettangolo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsFifgure
+{
+    internal class Rettangolo : Figura
+    {
+        public Rettangolo(double _base, double altezza) : base(_base, altezza)
+        {
+
+        }
+        public double CalcoloArea()
+        {
+            return Base * Altezza;
+        }
+        public double CalcoloPerimetro()
+        {
+            return (Base + Altezza) * 2;
+        }
+        public override string ToString()
+        {
+            return string.Format($"RETTANGOLO --> Base: {Base}, Altezza: {Altezza}, Area: {CalcoloArea()}, Perimetro: {CalcoloPerimetro()}");
+        }
+
+    }
+}
